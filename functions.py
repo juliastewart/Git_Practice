5713cfdae3364fb91c46155e0a6a2440582007b7
Julia Stewart <jstewart2@gradcenter.cuny.edu>
2024-08-14 15:44:34 -0400

    changed the contents and output of the instructor name and room number functions

--- functions.py
+++ functions.py
@@ -1,11 +1,18 @@
+
 # Here are some functions that you can fill in to make them work.
 # Replace the "raise NotImplementedError" line with the correct code!
    
-def print_the_instructors_name():
-    raise NotImplementedError
+def print_the_instructors_name(fn, ln):
+    if type(fn) is str:
+        name = str(fn+' '+ln)
+        print(name)
+    else:
+        raise NotImplementedError
+    return(name)
 
-def print_the_number_of_this_room():
-    raise NotImplementedError
+def print_the_number_of_this_room(num):
+    print('Room number: ', num)
+    return(num)
 
 def split_this_string_at_each_space(input_string):
     raise NotImplementedError
